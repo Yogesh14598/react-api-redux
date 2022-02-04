f561d99912ec07a283799e2b760a0201105901df
yogesh <yogeshh1498@gmail.com>
2022-02-04 09:06:25 +0530

    Updates

---
+++ precache-manifest.25036365715e1a12f0057251a9cbf948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89500827d395a7f4ba23a4f2d0935b95",
+    "url": "/react-api-redux/index.html"
+  },
+  {
+    "revision": "13b392ef769cf0e6fa79",
+    "url": "/react-api-redux/static/css/main.d42a5624.chunk.css"
+  },
+  {
+    "revision": "df36fde2cafdb7751db1",
+    "url": "/react-api-redux/static/js/2.2e2dbdfa.chunk.js"
+  },
+  {
+    "revision": "13b392ef769cf0e6fa79",
+    "url": "/react-api-redux/static/js/main.4c9e6f25.chunk.js"
+  },
+  {
+    "revision": "604b221773cb531b13af",
+    "url": "/react-api-redux/static/js/runtime~main.5a6ba6aa.js"
+  }
+]);
